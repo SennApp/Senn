478ee5d17e3e019f75ff43b77adc3f0a5de2737b
Ryota-S2 <147809275+Ryota-S2@users.noreply.github.com>
2024-07-11 12:34:06 +0900

    activity作っただけです

--- app/src/main/java/edu/konan_univ/senn/LeaderboardActivity.java
+++ app/src/main/java/edu/konan_univ/senn/LeaderboardActivity.java
@@ -5,44 +5,29 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
-import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-public class MainActivity extends AppCompatActivity {
-
-    private FirebaseAuth auth;
+public class LeaderboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SplashScreen.installSplashScreen(this);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_leaderboard);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
 
 
+
+
+
+
+
+
+
         });
-
-        // Initialize Firebase Auth
-        auth = FirebaseAuth.getInstance();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        auth.signInAnonymously().addOnCompleteListener(this, (task -> {
-            if (task.isSuccessful()) {
-                FirebaseUser user = auth.getCurrentUser();
-                // TODO: Do something
-            }
-        }));
     }
 }
