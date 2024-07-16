bd76f9a098dc20a587cb3c8e1243669d11657966
Hazuki <77273686+mano-hazuki@users.noreply.github.com>
2024-07-17 01:06:08 +0900

    Fixed wrong distance bug

--- app/src/main/java/edu/konan_univ/senn/CustomView.java
+++ app/src/main/java/edu/konan_univ/senn/CustomView.java
@@ -3,10 +3,9 @@ package edu.konan_univ.senn;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.graphics.PointF;
-import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -18,22 +17,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomView extends View {
-    List<PointF> points = new ArrayList<>();
-    Paint paint=new Paint();
-    boolean drawing=true;
-    double dist=0;
 
-    public CustomView(Context context,@Nullable AttributeSet attrs) {
-        super(context,attrs);
+    private final List<PointF> points = new ArrayList<>();
+    private final Paint paint = new Paint();
+    private boolean drawing = true;
+    private float dist = 0.0F;
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
     }
+
     @Override
-    protected void onDraw(@NonNull Canvas canvas){
-        if(points.size()<2){
+    protected void onDraw(@NonNull Canvas canvas) {
+        if (points.size() < 2) {
             return;
         }
         paint.setStrokeWidth(5);
-        for(int i=0;i<points.size()-1;i++) {
-                canvas.drawLine(points.get(i).x, points.get(i).y, points.get(i + 1).x, points.get(i + 1).y, paint);
+        for (int i = 0; i < points.size() - 1; i++) {
+            PointF current = points.get(i);
+            PointF next = points.get(i + 1);
+            canvas.drawLine(current.x, current.y, next.x, next.y, paint);
         }
     }
 
@@ -45,37 +48,50 @@ public class CustomView extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        PointF point=new PointF(event.getX(),event.getY());
-        switch(event.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                invalidate();
+        PointF point = new PointF(event.getX(), event.getY());
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                invalidate(); // 画面を更新
                 break;
-            case MotionEvent.ACTION_UP:
-                invalidate();
+            }
+            case MotionEvent.ACTION_UP: {
                 performClick();
-                drawing=false;
-                Log.d("kyori",String.valueOf(dist));
+                drawing = false;
+                Log.d("CustomView", dist + "cm");
+
+                invalidate(); // 画面を更新
                 break;
-            case MotionEvent.ACTION_MOVE:
-                if (drawing) {
-                    points.add(point);//点を配列に追加
-                    if(points.size()>=2) {
-                        dist += calcDist(points.get(points.size() - 1), points.get(points.size() - 2));
-                    }
-                    invalidate();
+            }
+            case MotionEvent.ACTION_MOVE: {
+                if (!drawing) {
+                    break;
                 }
+                points.add(point); // 点を配列に追加
+                updateDist();
+
+                invalidate(); // 画面を更新
                 break;
+            }
         }
         return true;
     }
 
-    private double calcDist(PointF a,PointF b){
-        double ans=0;
-        Log.d("zahyouX",String.valueOf(a.x));
-        Log.d("zahyouY",String.valueOf(a.y));
-        ans=Math.sqrt(Math.pow((a.x-b.x)*getScaleX(),2)+Math.pow((a.y-b.y)*getScaleY(),2));
-        ans*=0.0264583333;
-        return ans;
+    private void updateDist() {
+        if (points.size() >= 2) {
+            PointF current = points.get(points.size() - 1);
+            PointF prev = points.get(points.size() - 2);
+            dist += calcDist(current, prev);
+        }
     }
 
+    private float calcDist(PointF a, PointF b) {
+        float cmPerInches = 2.54F;
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        float inchesX = Math.abs(a.x - b.x) / metrics.xdpi;
+        float inchesY = Math.abs(a.y - b.y) / metrics.ydpi;
+        float cmX = inchesX * cmPerInches;
+        float cmY = inchesY * cmPerInches;
+//        Log.d("calcDist", "X: " + a.x + "," + " " + "Y: " + a.y);
+        return (float) Math.sqrt(Math.pow(cmX, 2) + Math.pow(cmY, 2));
+    }
 }
--- app/src/main/java/edu/konan_univ/senn/MainActivity.java
+++ app/src/main/java/edu/konan_univ/senn/MainActivity.java
@@ -3,8 +3,6 @@ package edu.konan_univ.senn;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
